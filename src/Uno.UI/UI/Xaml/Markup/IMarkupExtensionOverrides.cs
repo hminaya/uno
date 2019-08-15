b1037018a83367a9eaf917c07c4c1848ac80d402
Massimo <Massimo.Cacchiotti@nventive.com>
2019-08-15 11:56:24 -0400

    Added first set of changes to support custom MarkupExtension

---
+++ src/Uno.UI/UI/Xaml/Markup/IMarkupExtensionOverrides.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno.UI.Xaml.Markup.MarkupExtension
+{
+	public partial interface IMarkupExtensionOverrides
+	{
+		object ProvideValue();
+	}
+}
